cc42fda4117268e2326e60aeb7a2725f275442b8
BackEnd5 <rlwnsdlek12@gmail.com>
2023-03-18 11:23:56 +0900

    feature_git에서 추가요22

--- src/GitClass.java
+++ src/GitClass.java
@@ -2,5 +2,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("하이하이요");
         System.out.println("feature_git에서 추가요");
+        System.out.println("feature_git에서 추가요22");
     }
 }
